e15a9283f01ef8c1ecf564750904a8a408198e25
priamoryki <lymarpasha@gmail.com>
2021-07-10 00:01:22 +0300

    Tests_added

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ This is Sudoku-Solver based on Python. At the moment, only the usual solution [a
 You can see their essence here when I get my hands on them :).  
 
 # Code pros and cons
-* The code is not divided into classes (I will fix this when I add a convenient user interface)
 * There are places in which the code is repeated (it must be put into a separate function)
 * Generalized to large Sudoku boards
 * It's pretty clear what's going on inside the various functions
--- src/SudokuSolver.py
+++ src/SudokuSolver.py
@@ -26,7 +26,7 @@ def default_solution(board):
     """
     n = int(sqrt(len(board)))
     board = deepcopy(board)
-    posboard = [[[k for k in range(1, n ** 2 + 1)] if board[i][j] == 0 else [] for j in range(n ** 2)] for i in range(n ** 2)]
+    posboard = [[[*range(1, n ** 2 + 1)] if board[i][j] == 0 else [] for j in range(n ** 2)] for i in range(n ** 2)]
 
     def num_of_empty_sells():
         res = 0
@@ -50,7 +50,7 @@ def default_solution(board):
             pass
 
     def get_shifts(v):
-        result = [i for i in range(1, n)]
+        result = [*range(1, n)]
         shift = v % n + 1
         for i in range(len(result)):
             if (result[i] == shift):
@@ -66,7 +66,6 @@ def default_solution(board):
             update_posboard(i, j, board[i][new_j])
 
         i_shifts, j_shifts = get_shifts(i), get_shifts(j)
-
         for i_shift in i_shifts:
             for j_shift in j_shifts:
                 update_posboard(i, j, board[i + i_shift][j + j_shift])
@@ -99,6 +98,30 @@ def default_solution(board):
             if (is_change):
                 posboard[i][j] = [value]
 
+            is_change = True
+            i_shifts, j_shifts = get_shifts(i), [0, *get_shifts(j)]
+            for i_shift in i_shifts:
+                for j_shift in j_shifts:
+                    if (value in posboard[i + i_shift][j + j_shift]):
+                        is_change = False
+
+            if (is_change):
+                for new_j in range(n ** 2):
+                    if ((new_j - j) not in j_shifts):
+                        update_posboard(i, new_j, value)
+
+            is_change = True
+            i_shifts, j_shifts = [0, *get_shifts(i)], get_shifts(j)
+            for i_shift in i_shifts:
+                for j_shift in j_shifts:
+                    if (value in posboard[i + i_shift][j + j_shift]):
+                        is_change = False
+
+            if (is_change):
+                for new_i in range(n ** 2):
+                    if ((new_i - i) not in i_shifts):
+                        update_posboard(new_i, j, value)
+
     def fill(i, j):
         if (len(posboard[i][j]) == 1):
             board[i][j] = posboard[i][j].pop()
@@ -124,8 +147,7 @@ def dfs_solution(board):
             for j in range(n ** 2):
                 if (board[i][j] == 0):
                     continue
-                box = i // n * n + j // n
-                tmp = int(board[i][j]) - 1
+                box, tmp = i // n * n + j // n, board[i][j] - 1
                 row_constrain[i][tmp], col_constrain[j][tmp], box_constrain[box][tmp] = 1, 1, 1
         dfs(0, 0)
 
@@ -141,7 +163,7 @@ def dfs_solution(board):
             if (row_constrain[r][i] or col_constrain[c][i] or box_constrain[box][i]):
                 continue
             row_constrain[r][i], col_constrain[c][i], box_constrain[box][i] = 1, 1, 1
-            board[r][c] = str(i + 1)
+            board[r][c] = i + 1
             if (dfs(next_r, next_c)):
                 return True
             board[r][c] = 0
@@ -207,6 +229,17 @@ def dfs_solution(board):
     4 0 6 0 0 0 2 0 0
     0 0 7 4 0 0 0 0 0
     0 0 0 0 2 5 6 0 0
+    
+    EVIL TEST
+    5 0 0 0 3 0 0 0 0
+    0 0 0 0 0 0 0 4 1
+    0 0 0 0 0 0 0 0 0
+    0 0 0 1 0 4 7 0 0
+    6 0 0 0 0 0 5 0 0
+    0 0 0 8 0 0 0 0 0
+    0 8 0 2 0 7 0 0 0
+    0 0 0 0 6 0 3 9 0
+    0 1 0 0 0 0 0 0 0
 
 4x4:
     13 0  0  7  0  0  0  0  0  5  14 2  0  0  9  0
@@ -228,9 +261,21 @@ def dfs_solution(board):
 '''
 
 
-n = 3
-board = read_board(n)
+if (__name__ == '__main__'):
+    n = 3
+    board = [
+        [0, 0, 5, 2, 8, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 4, 1, 0, 0],
+        [0, 0, 9, 0, 0, 0, 4, 0, 3],
+        [9, 0, 0, 7, 0, 0, 0, 6, 0],
+        [0, 8, 0, 0, 1, 0, 0, 4, 0],
+        [0, 5, 0, 0, 0, 9, 0, 0, 1],
+        [4, 0, 6, 0, 0, 0, 2, 0, 0],
+        [0, 0, 7, 4, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 5, 6, 0, 0]
+    ]
+    # board = read_board(n)
 
-print_board(default_solution(board))
+    print_board(default_solution(board))
 
-#print_board(dfs_solution(board))
+    # print_board(dfs_solution(board))
